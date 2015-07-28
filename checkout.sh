74cf0c6e68e92236c387f8f0e5864e13280ae48e
Laszlo Dobos <dobos@complex.elte.hu>
2015-07-28 15:52:11 -0400

    Check-out script added to switch submodules to default branch

---
+++ checkout.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#git clone --recursive --branch v1.0/develop git@github.com:idies/skyquery-all.git
+
+cd graywulf
+git checkout -b v1.0/develop remotes/origin/v1.1/develop --
+
+cd ../graywulf-plugins
+git checkout -b v1.0/develop remotes/origin/v1.1/develop --
+
+cd ../graywulf-tools
+git checkout -b v1.0/develop remotes/origin/v1.1/develop --
+
+cd ../skyquery
+git checkout -b v1.0/develop remotes/origin/v1.1/develop --
+
+cd ../skyquery-config
+git checkout -b v1.0/develop remotes/origin/v1.1/develop --
+
+cd ..
