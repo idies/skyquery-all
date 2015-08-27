52f9910aaa3f41f5b7bfb14344095b5c31819167
Laszlo Dobos <dobos@complex.elte.hu>
2015-08-27 10:55:00 -0400

    Config updated to new login portal.

--- checkout.sh
+++ checkout.sh
@@ -2,18 +2,18 @@
 #git clone --recursive --branch v1.0/develop git@github.com:idies/skyquery-all.git
 
 cd graywulf
-git checkout -b v1.0/develop remotes/origin/v1.1/develop --
+git checkout -b v1.0/develop remotes/origin/v1.0/develop --
 
 cd ../graywulf-plugins
-git checkout -b v1.0/develop remotes/origin/v1.1/develop --
+git checkout -b v1.0/develop remotes/origin/v1.0/develop --
 
 cd ../graywulf-tools
-git checkout -b v1.0/develop remotes/origin/v1.1/develop --
+git checkout -b v1.0/develop remotes/origin/v1.0/develop --
 
 cd ../skyquery
-git checkout -b v1.0/develop remotes/origin/v1.1/develop --
+git checkout -b v1.0/develop remotes/origin/v1.0/develop --
 
 cd ../skyquery-config
-git checkout -b v1.0/develop remotes/origin/v1.1/develop --
+git checkout -b v1.0/develop remotes/origin/v1.0/develop --
 
 cd ..
