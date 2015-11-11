c6168a21d68a5eb68376aef88f35e3945b2ed790
Laszlo Dobos <dobos@complex.elte.hu>
2015-11-11 12:07:23 +0100

    Updated to track remotes on the right branch. Force branch overwrite if local branch exists.

--- checkout.sh
+++ checkout.sh
@@ -2,21 +2,24 @@
 #git clone --recursive --branch v1.1/develop git@github.com:idies/skyquery-all.git
 
 cd graywulf
-git checkout -b v1.1/develop remotes/origin/v1.1/develop --
+git checkout -B v1.1/develop remotes/origin/v1.1/develop --
 
 cd ../graywulf-plugins
-git checkout -b v1.1/develop remotes/origin/v1.1/develop --
+git checkout -B v1.1/develop remotes/origin/v1.1/develop --
 
 cd ../graywulf-tools
-git checkout -b v1.1/develop remotes/origin/v1.1/develop --
+git checkout -B v1.1/develop remotes/origin/v1.1/develop --
+
+cd ../sharpfitsio
+git checkout -B develop remotes/origin/develop --
 
 cd ../skyquery
-git checkout -b v1.1/develop remotes/origin/v1.1/develop --
+git checkout -B v1.1/develop remotes/origin/v1.1/develop --
 
 cd ../skyquery-config
-git checkout -b v1.1/develop remotes/origin/v1.1/develop --
+git checkout -B v1.1/develop remotes/origin/v1.1/develop --
 
 cd ../skyquery-skynodes
-git checkout -b v1.1/develop remotes/origin/v1.1/develop --
+git checkout -B v1.1/develop remotes/origin/v1.1/develop --
 
 cd ..
