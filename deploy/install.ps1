7789e9516f4fe7a04f3276d011c81ba9a3e34e2e
Laszlo Dobos <dobos@complex.elte.hu>
2016-02-16 11:02:58 -0500

    Added registry and scheduler steps to install script.

--- deploy/install.ps1
+++ deploy/install.ps1
@@ -1,7 +1,7 @@
 ﻿# Installs SkyQuery binary components and web sites for the first time
 # before execution, make sure to dot source an appropriate config file:
 # . .\skyquery-config\scidev01\configure.ps1
-# .\deploy\reinstall.ps1
+# .\deploy\install.ps1
 
 $fwpath = [System.Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()
 
@@ -10,6 +10,15 @@ if ($skyquery_deployscheduler -or $skyquery_deployremoteservice) {
 	$cred = Get-Credential $skyquery_serviceaccount
 	$user = $cred.UserName
 	$pass = $cred.GetNetworkCredential().Password
+}
+
+# Create and configure registry
+if ($skyquery_deployregistry) {
+	echo "Creating registry..."
+	& .\bin\$skyquery_target\gwregutil.exe createdb
+	& .\bin\$skyquery_target\gwregutil.exe createcluster -cluster Graywulf -User admin -Email admin@graywulf.org -Password alma
+	sqlcmd -S $skyquery_registrysql -d $skyquery_registrydb -Q "CREATE USER [$user] FOR LOGIN [$user]"
+	sqlcmd -S $skyquery_registrysql -d $skyquery_registrydb -Q "ALTER ROLE [db_owner] ADD MEMBER [$user]"
 }
 
 # Copy binaries
@@ -33,9 +42,6 @@ if ($skyquery_deployremoteservice) {
 	} -Args $user, $pass, $skyquery_gwbin, $fwpath, $skyquery_remoteservice
 }
 
-# Install scheduler
-# TODO
-
 # Start remote service
 if ($skyquery_deployremoteservice) {
 	echo "Starting remoting service '$skyquery_remoteservice' on all servers..."
@@ -45,11 +51,32 @@ if ($skyquery_deployremoteservice) {
 	} -Args $skyquery_remoteservice
 }
 
-# Start scheduler
-# TODO
+# Install scheduler
+if ($skyquery_deployscheduler) {
+	echo "Installing scheduler"
+	icm $skyquery_scheduler_nodes -Script {
+		param($un, $pw, $gw, $fw, $sn) 
+		& $fw\InstallUtil.exe /username=$un /password=$pw /unattended /svcname=$sn C:\$gw\gwscheduler.exe
+	} -Args $user, $pass, $skyquery_gwbin, $fwpath, $skyquery_schedulerservice
+}
 
+# Start scheduler
+if ($skyquery_deployscheduler) {
+	echo "Starting scheduler service '$skyquery_schedulerservice' on the controllers..."
+	icm $skyquery_scheduler_nodes -Script { 
+		param($sn) 
+		net start $sn 
+	} -Args $skyquery_schedulerservice
+}
 
 # Copy web site
-# TODO
-#cp C:\Data\dobos\project\skyquery-all\bin \\scitest02\data\dobos\project\skyquery-all -Force -Recurse
-#cp C:\Data\dobos\project\skyquery-all\www \\scitest02\data\dobos\project\skyquery-all -Force -Recurse
+if ($skyquery_deploywww) {
+	echo "Copying web site..."
+	foreach ( $s in $skyquery_wwwserver) {
+		if (-Not (Test-Path \\$s\$skyquery_www)) {
+			mkdir \\$s\$skyquery_www
+		}
+		rm -force -recurse \\$s\$skyquery_www\*
+		cp .\www\* \\$s\$skyquery_www -recurse -force 
+	}
+}
