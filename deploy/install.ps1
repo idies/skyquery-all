f44d4b42ee8d5b0d4fb497718d8a961303551576
Laszlo Dobos <dobos@complex.elte.hu>
2016-08-04 18:58:13 -0400

    Working on pre-prod installer.

--- deploy/install.ps1
+++ deploy/install.ps1
@@ -1,25 +1,92 @@
 ﻿# Installs SkyQuery binary components and web sites for the first time
-# before execution, make sure to dot source an appropriate config file:
-# . .\skyquery-config\scidev01\configure.ps1
-# .\deploy\install.ps1
+# .\deploy\install.ps1 [config]
 
+function ExitOnError() {
+	if ($LastExitCode -gt 0) { 
+		exit 
+	}
+}
+
+function FindMachines($role) {
+	$csb = New-Object System.Data.SqlClient.SqlConnectionStringBuilder
+	$csb["Data Source"] = $skyquery_registrysql
+	$csb["Initial Catalog"] = $skyquery_registrydb
+	$csb["MultipleActiveResultsets"] = $TRUE
+	$csb["Integrated Security"] = $TRUE
+	[Jhu.Graywulf.Registry.ContextManager]::Instance.ConnectionString = $csb.ConnectionString
+	$context = [Jhu.Graywulf.Registry.ContextManager]::Instance.CreateContext()
+	$ef = New-Object Jhu.Graywulf.Registry.EntityFactory $context
+
+	$mr = $ef.LoadEntity($role)
+	$mr.LoadMachines($TRUE)
+	$mm = $mr.Machines.Values | select -ExpandProperty HostName | select -ExpandProperty ResolvedValue
+	
+	$context.Dispose()
+	
+	return $mm
+}
+
+# Import references
+Add-Type -Path ".\bin\Debug\Jhu.Graywulf.Registry.dll"
+
+# Stop on any error
+$ErrorActionPreference = "Stop"
 $fwpath = [System.Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()
 
+# Load config
+$config = $args[0] 
+. .\$config\configure.ps1
+
 # Prompt for user account to be used for service install
-if ($skyquery_deployscheduler -or $skyquery_deployremoteservice) {
+if ($FALSE) { # ($skyquery_deployscheduler -or $skyquery_deployremoteservice) {
 	$cred = Get-Credential $skyquery_serviceaccount
 	$user = $cred.UserName
 	$pass = $cred.GetNetworkCredential().Password
+} else {
+	$user = $skyquery_usergroup
 }
 
 # Create and configure registry
-if ($skyquery_deployregistry) {
+if ($FALSE) { #($skyquery_deployregistry) {
+	echo "Creating log..."
+	& .\bin\$skyquery_target\gwregutil.exe CreateLog -Q -Username "$user" -Role "db_owner"
+	ExitOnError
+	
+	echo "Creating job persistence store..."
+	& .\bin\$skyquery_target\gwregutil.exe CreateJobPersistence -Q -Username "$user" -Role "db_owner"
+	ExitOnError
+	
 	echo "Creating registry..."
-	& .\bin\$skyquery_target\gwregutil.exe createdb
-	& .\bin\$skyquery_target\gwregutil.exe createcluster -cluster Graywulf -User admin -Email admin@graywulf.org -Password alma
-	sqlcmd -S $skyquery_registrysql -d $skyquery_registrydb -Q "CREATE USER [$user] FOR LOGIN [$user]"
-	sqlcmd -S $skyquery_registrysql -d $skyquery_registrydb -Q "ALTER ROLE [db_owner] ADD MEMBER [$user]"
+	& .\bin\$skyquery_target\gwregutil.exe CreateRegistry -Q -Username "$user" -Role "db_owner"
+	ExitOnError
+	& .\bin\$skyquery_target\gwregutil.exe AddCluster -Q -cluster "Graywulf" -User admin -Email admin@graywulf.org -Password alma
+	ExitOnError
+	& .\bin\$skyquery_target\gwregutil.exe AddDomain -Q -cluster "Cluster:Graywulf" -Domain "SciServer"
+	ExitOnError
+	
+	echo "Installing SkyQuery..."
+	& .\bin\$skyquery_target\sqregutil.exe install -Domain "Domain:Graywulf\SciServer"
+	ExitOnError
+	
+	echo "Importing registry: cluster..."
+	& .\bin\$skyquery_target\gwregutil.exe Import -Input .\$config\SkyQuery_Cluster.xml -Duplicates Update
+	ExitOnError
+	echo "Importing registry: federation..."
+	& .\bin\$skyquery_target\gwregutil.exe Import -Input .\$config\SkyQuery_Federation.xml -Duplicates Update
+	ExitOnError
+	echo "Importing registry: layout..."
+	& .\bin\$skyquery_target\gwregutil.exe Import -Input .\$config\SkyQuery_Layout.xml -Duplicates Update
+	ExitOnError
 }
+
+# Find servers
+echo "Find servers..."
+$skyquery_controller = FindMachines("MachineRole:Graywulf\Controller")
+$skyquery_controller
+$skyquery_skynode = FindMachines("MachineRole:Graywulf\SkyNode")
+$skyquery_skynode
+
+exit
 
 # Copy binaries
 if ($skyquery_deployscheduler -or $skyquery_deployremoteservice) {
