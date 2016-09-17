ddb7415a87740b372ed69d3126de7147b631a3f2
Laszlo Dobos <dobos@complex.elte.hu>
2016-09-17 09:20:37 -0400

    Fixed assembly locking issue in deploy script.

--- deploy/setup.ps1
+++ deploy/setup.ps1
@@ -1,5 +1,21 @@
-$verb = $args[0]
-$config = $args[1]
+# Source install libraries
+. .\deploy\instutil.ps1
+. .\deploy\instlib.ps1
+
+# To prevent locking assemblies, the script executes itself
+# in a saparate poweshell session
+
+if ($args[0] -notmatch "-i") {
+	$verb = $args[0]
+	$config = $args[1]
+	powershell -Command "$PSCommandPath -i $verb $config"
+	exit
+}
+
+# This is the normal execution path
+
+$verb = $args[1]
+$config = $args[2]
 
 # Load config
 if (!(test-path .\$config\configure.ps1)) {
@@ -7,12 +23,11 @@ if (!(test-path .\$config\configure.ps1)) {
 	exit
 }
 
+# Load configuration and initialize setup
+
 . .\$config\configure.ps1
 Write-Host "Configured for $skyquery_config"
 
-# Initialize install libraries
-. .\deploy\instutil.ps1
-. .\deploy\instlib.ps1
 Init
 InitRegistry
 
