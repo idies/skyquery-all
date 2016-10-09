81a60fc302c8bb5296057d3b8add10cb04edc063
Laszlo Dobos <dobos@complex.elte.hu>
2016-10-09 07:20:25 -0400

    Check-out script updated.

--- checkout.sh
+++ checkout.sh
@@ -2,25 +2,25 @@
 #git clone --recursive --branch v1.1/develop git@github.com:idies/skyquery-all.git
 
 cd graywulf
-git checkout -B v1.1/develop remotes/origin/v1.1/develop --
+git checkout -B v1.2/develop remotes/origin/v1.2/develop --
 
 cd ../graywulf-plugins
-git checkout -B v1.1/develop remotes/origin/v1.1/develop --
+git checkout -B v1.2/develop remotes/origin/v1.2/develop --
 
 cd ../graywulf-tools
-git checkout -B v1.1/develop remotes/origin/v1.1/develop --
+git checkout -B v1.2/develop remotes/origin/v1.2/develop --
 
 cd ../sharpfitsio
 git checkout -B develop remotes/origin/develop --
 
 cd ../skyquery
-git checkout -B v1.1/develop remotes/origin/v1.1/develop --
+git checkout -B v1.2/develop remotes/origin/v1.2/develop --
 
 cd ../skyquery-config
-git checkout -B v1.1/develop remotes/origin/v1.1/develop --
+git checkout -B v1.2/develop remotes/origin/v1.2/develop --
 
 cd ../skyquery-skynodes
-git checkout -B v1.1/develop remotes/origin/v1.1/develop --
+git checkout -B v1.2/develop remotes/origin/v1.2/develop --
 
 cd ../spherical
 git checkout -B develop remotes/origin/develop --
