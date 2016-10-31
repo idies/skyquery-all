1f3fee58512e6e37672a2f34371d3ddb2ac3135d
Laszlo Dobos <dobos@complex.elte.hu>
2016-10-31 18:25:53 -0400

    Switched to correct branch

--- checkout.sh
+++ checkout.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 #git clone --recursive --branch v1.1/develop git@github.com:idies/skyquery-all.git
 
+GRAYWULF="v1.2/develop"
+SKYQUERY="v1.2/develop"
+SHARPFITSIO="develop"
+SPHERICAL="develop"
+
 cd graywulf
-git checkout -B v1.2/develop remotes/origin/v1.2/develop --
+git checkout -B $GRAYWULF remotes/origin/$GRAYWULF --
 
 cd ../graywulf-build
-git checkout -B v1.2/develop remotes/origin/v1.2/develop --
+git checkout -B $GRAYWULF remotes/origin/$GRAYWULF --
 
 cd ../graywulf-plugins
-git checkout -B v1.2/develop remotes/origin/v1.2/develop --
+git checkout -B $GRAYWULF remotes/origin/$GRAYWULF --
 
 cd ../graywulf-tools
-git checkout -B v1.2/develop remotes/origin/v1.2/develop --
+git checkout -B $GRAYWULF remotes/origin/$GRAYWULF --
+
+cd ../graywulf-sciserver-init
+git checkout -B $GRAYWULF remotes/origin/$GRAYWULF --
 
 cd ../sharpfitsio
-git checkout -B develop remotes/origin/develop --
+git checkout -B $SHARPFITSIO remotes/origin/$SHARPFITSIO --
 
 cd ../skyquery
-git checkout -B v1.2/develop remotes/origin/v1.2/develop --
+git checkout -B $SKYQUERY remotes/origin/$SKYQUERY --
 
 cd ../skyquery-config
-git checkout -B v1.2/develop remotes/origin/v1.2/develop --
+git checkout -B $SKYQUERY remotes/origin/$SKYQUERY --
 
 cd ../skyquery-skynodes
-git checkout -B v1.2/develop remotes/origin/v1.2/develop --
+git checkout -B $SKYQUERY remotes/origin/$SKYQUERY --
 
 cd ../spherical
-git checkout -B develop remotes/origin/develop --
+git checkout -B $SPHERICAL remotes/origin/$SPHERICAL --
 
 cd ..
