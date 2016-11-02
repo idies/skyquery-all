1a6f7e1bb68ad86ea552c383138e4a6878ec4013
Laszlo Dobos <dobos@complex.elte.hu>
2016-11-02 12:28:29 -0400

    Updated build script.

--- deploy/build.ps1
+++ deploy/build.ps1
@@ -1,22 +1,37 @@
 param (
-	[string]$config = "Debug"
+	[string]$root = "config\default"
 )
+
+# Set config root
+$configfile = Get-Item gwconfig.xml
+[xml]$xml = Get-Content $configfile
+$xml.config.root = $root
+$xml.Save($configfile.FullName)
+
+. $root\configure.ps1
 
 $SolutionDir=pwd
 
-& "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" `
-	/P:SolutionDir="$SolutionDir\" `
-	/P:SolutionName="skyquery-all" `
-	/p:Configuration="$config" `
-	/consoleloggerparameters:"Summary" `
-	graywulf-build\src\Jhu.Graywulf.Build.ConfigUtil\Jhu.Graywulf.Build.ConfigUtil.csproj
+echo "Starting build for config '$root' with target '$skyquery_target'"
+echo "Building config tool..."
 
 & "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" `
 	/target:rebuild `
-	/verbosity:normal `
-	/detailedsummary `
 	/maxcpucount:16 `
-	/consoleloggerparameters:"Summary" `
+	/P:SolutionDir="$SolutionDir\" `
+	/P:SolutionName="skyquery-all" `
+	/p:Configuration="$skyquery_target" `
+	/clp:Summary `
+	/verbosity:normal `
+	"graywulf-build\src\Jhu.Graywulf.Build.ConfigUtil\Jhu.Graywulf.Build.ConfigUtil.csproj"
+
+echo "Building SkyQuery..."
+
+& "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" `
+	/target:rebuild `
+	/maxcpucount:16 `
+	/verbosity:normal `
+	/clp:"Summary;EnableMPLogging" `
 	/fileLogger /fileloggerparameters:ForceNoAlign `
-	/p:Configuration="$config" `
-	skyquery-all.sln
+	/p:Configuration="$skyquery_target" `
+	"skyquery-all.sln"
