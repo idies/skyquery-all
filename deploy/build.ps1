6c5837dbbf3912adeea6a2db479e58e7236a2309
Laszlo Dobos <dobos@complex.elte.hu>
2016-11-08 14:59:11 -0500

    SQL CLR scripting tool moved.

--- deploy/build.ps1
+++ deploy/build.ps1
@@ -25,6 +25,18 @@ echo "Building config tool..."
 	/verbosity:normal `
 	"graywulf-build\src\Jhu.Graywulf.Build.ConfigUtil\Jhu.Graywulf.Build.ConfigUtil.csproj"
 
+echo "Building SQL CLR scripting tool..."
+
+& "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" `
+	/target:rebuild `
+	/maxcpucount:16 `
+	/P:SolutionDir="$SolutionDir\" `
+	/P:SolutionName="skyquery-all" `
+	/p:Configuration="$skyquery_target" `
+	/clp:Summary `
+	/verbosity:normal `
+	"graywulf-build\src\Jhu.Graywulf.SqlClrUtil\Jhu.Graywulf.SqlClrUtil.csproj"
+
 echo "Building SkyQuery..."
 
 & "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" `
